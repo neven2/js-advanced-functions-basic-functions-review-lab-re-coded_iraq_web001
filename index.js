3e391b07c81227db1ef470052350ce06411c4f9a
Neven Bedr <neven2225@gmail.com>
2020-06-12 10:28:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function saturdayFun(myjob='roller-skate'){
+  return (`This Saturday, I want to ${myjob}!`);
+}
+
+function mondayWork (activity='go to the office'){
+  return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective (anything='*'){
+  return function human(word='special'){
+    return `You are ${anything}${word}${anything}!`;
+  }
+}
+
+const Calculator ={
+  add: (a,b) => a+b,
+  subtract: (a,b) => a-b,
+  multiply: (a,b) => a*b,
+  divide: (a,b) => a/b,
+}
+
+
+function actionApplyer(num, arrays){
+  if(arrays.length === 0)
+    return num
+  else {
+    return ((num * 2) + 1000) % 7
+  }
+}
